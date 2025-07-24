4bf96e5c9fc82a13148de2f5333f1d7192c18738
st21123 <st21123@howick.school.nz>
2025-07-24 12:01:00 +1200

    GUI setup and main menu
    This code is the base of my GUI for all the frames, it's set up for things to be added easily in the future. I also created the first  frame which is the MainMenu Frame, the buttons on the main menu frame don't work bc the method for the command isn't created yet. It's also missing the "would you like to choose from" and the image.

---
+++ Perfuminator_V1.py
@@ -0,0 +1,66 @@
+'''
+An app that where you can make your own perfume
+
+Version 1: basic working program, functions, no optimisations, no validation at all
+ '''
+
+from tkinter import *
+
+class PerfumeMaker:
+    '''Set up the GUI'''
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Perfumantor")
+        self.root.grid_rowconfigure(0,weight=1)
+        self.root.grid_columnconfigure(0,weight=1)
+
+        # Container for frames
+        self.container = Frame(self.root)
+        self.container.grid(row=0, column=0, sticky=NSEW)
+        self.container.grid_rowconfigure(0,weight=1)
+        self.container.grid_columnconfigure(0,weight=1)
+
+        # Dictionary to hold frames
+        self.frames = {}
+        self.frames["MainMenu"] = self.create_main_menu()
+
+
+        # Show the initial frame
+        self.show_frame("MainMenu")
+
+
+    def run(self):
+        '''runs the program'''
+        self.root.mainloop()
+
+    def show_frame(self, name):
+        '''Display the required frame from the dictionary'''
+        frame = self.frames[name]
+        frame.tkraise()
+
+    def create_main_menu(self):
+        '''Create the mainframe'''
+        
+        frame = Frame(self.container)
+        frame.grid(row=0, column=0, sticky=NSEW)
+        
+        for i in range(2):
+            frame.grid_rowconfigure(i, weight=1)
+        for j in range(2):
+            frame.grid_columnconfigure(j, weight=1)            
+
+        heading = Label(frame, text="Welcome to the Perfumanator", font="Verdana 16 bold")
+        heading.grid(row=0, column=0, columnspan=2, sticky=NSEW)
+
+        free_reign_button = Button(frame, text="Free Reign", bg="lightgreen", font="Verdana 12 bold",command=lambda: self.show_frame("..."))
+        free_reign_button.grid(row=1, column=0, sticky=NSEW)
+        
+        preset_palette_button= Button(frame, text="Preset Palette", bg="yellow", font="Verdana 12 bold",command=lambda: self.show_frame("..."))
+        preset_palette_button.grid(row=1, column=1, sticky=NSEW)
+
+        return frame
+
+
+if __name__ == "__main__":
+    app = PerfumeMaker()
+    app.run()
