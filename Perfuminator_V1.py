8641e98375c356b7837168b52d64b71f5262facf
st21123 <st21123@howick.school.nz>
2025-07-24 12:13:18 +1200

    Finished Setup
    I Created All Methods to create each frame and I also did the main menu with the buttons but not with the image. Created spacing for where the image will go

--- Perfuminator_V1.py
+++ Perfuminator_V1.py
@@ -23,7 +23,9 @@ class PerfumeMaker:
         # Dictionary to hold frames
         self.frames = {}
         self.frames["MainMenu"] = self.create_main_menu()
-
+        self.frames["MainGame"] = self.create_main_game
+        self.frames["PaletteSelector"] = self.create_palette_selector
+        self.frames["Checkout"] = self.create_checkout
 
         # Show the initial frame
         self.show_frame("MainMenu")
@@ -52,15 +54,27 @@ class PerfumeMaker:
         heading = Label(frame, text="Welcome to the Perfumanator", font="Verdana 16 bold")
         heading.grid(row=0, column=0, columnspan=2, sticky=NSEW)
 
-        free_reign_button = Button(frame, text="Free Reign", bg="lightgreen", font="Verdana 12 bold",command=lambda: self.show_frame("..."))
-        free_reign_button.grid(row=1, column=0, sticky=NSEW)
-        
-        preset_palette_button= Button(frame, text="Preset Palette", bg="yellow", font="Verdana 12 bold",command=lambda: self.show_frame("..."))
-        preset_palette_button.grid(row=1, column=1, sticky=NSEW)
+        main_menu_text = Label(frame, text="would you like to choose from", font="Verdana 11")
+        main_menu_text.grid(row=1, column=0, columnspan=2, sticky=NSEW, pady=(50, 0))
+
+        free_reign_button = Button(frame, text="Free Reign", bg="lightgreen", font="Verdana 12 bold",command=lambda: self.show_frame("MainGame"))
+        free_reign_button.grid(row=2, column=0, sticky=NSEW)
+    
+        preset_palette_button= Button(frame, text="Preset Palette", bg="yellow", font="Verdana 12 bold",command=lambda: self.show_frame("PaletteSelector"))
+        preset_palette_button.grid(row=2, column=1, sticky=NSEW)
 
         return frame
+    
+    def create_main_game(self):
+        pass
 
+    def create_palette_selector():
+        pass
 
+    def create_checkout():
+        pass
+
+    
 if __name__ == "__main__":
     app = PerfumeMaker()
     app.run()
