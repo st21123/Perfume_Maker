4474488b516a241c52f0a2c235f0966c257169e1
st21123 <st21123@howick.school.nz>
2025-08-13 13:36:13 +1200

    New FrameManager Class
    As I decided to create different classes for the different frames I need to do an entire overhaul on how my GUI system worked. To do this I created another class called Frame Manager. This class is the parent class for all of my "subclasses" which are the different frames. like how I did it before I created a frame to hold all of the other frames and raised them using frame.tkraise. In this class as this will be the class that is being "run" and the parent of the other classes I made it load the scent_data from a JSON file containing all of the scents. Since I added this I needed to reformat the rest of the game. the MainMenu Class is just a copy paste of it before with constructing also from the parent, maingame is the same and pretty much unchanged. and the paleteeSelector as that will use different methods now I have just # it out and will work on it later.

--- Perfuminator_V1.py
+++ Perfuminator_V1.py
@@ -5,116 +5,109 @@ Version 1: basic working program, functions, no optimisations, no validation at 
  '''
 
 from tkinter import *
+from tkinter.ttk import Combobox
+import json
 
-class PerfumeMaker:
-    '''Set up the GUI'''
+class FrameManager(Tk):
     def __init__(self):
-        self.root = Tk()
-        self.root.title("Perfumantor")
-        self.root.grid_rowconfigure(0,weight=1)
-        self.root.grid_columnconfigure(0,weight=1)
-        self.root.geometry("500x500")
+        super().__init__()
+        self.title("Perfumantor")
+        self.minsize(800,550)
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        # Container for frames
-        self.container = Frame(self.root)
-        self.container.grid(row=0, column=0, sticky=NSEW)
-        self.container.grid_rowconfigure(0,weight=1)
-        self.container.grid_columnconfigure(0,weight=1)
+        self.scent_notes_data ={}
+        self.palettes_data ={}
+        self.load_scent_data("scent_data.json")
 
-        # Dictionary to hold frames
-        self.frames = {}
-        self.frames["MainMenu"] = self.create_main_menu()
-        self.frames["PaletteSelector"] = self.create_palette_selector()
-        self.frames["MainGame"] = self.create_main_game()
-        self.frames["Checkout"] = self.create_checkout()
+        self.container = Frame(self)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
-        # Show the initial frame
+        self.frames ={}
+        self.frames["MainMenu"] = MainMenu(self.container, self)
+        self.frames["PaletteSelector"] = PaletteSelector(self.container, self)
+        self.frames["Checkout"] = Checkout(self.container, self)
+
         self.show_frame("MainMenu")
 
-
-    def run(self):
-        '''runs the program'''
-        self.root.mainloop()
+    def load_scent_data(self, filename):
+        '''Loads scent notes and palettes from a JSON file.'''
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            self.scent_notes_data = data.get("scent_notes", {})
+            self.palettes_data = data.get("palettes", {})
 
     def show_frame(self, name):
         '''Display the required frame from the dictionary'''
         frame = self.frames[name]
         frame.tkraise()
+    
+    def start_main_game(self, palette_type):
+        '''Initializes and displays the MainGame frame with the selected palette.'''
+        if palette_type == "free_reign":
+            selected_palette = list(self.scent_notes_data.keys())
+        else:
+            selected_palette = self.palettes_data.get(palette_type, [])
 
-    def create_main_menu(self):
-        '''Create the mainframe'''
-        
-        frame = Frame(self.container)
-        frame.grid(row=0, column=0, sticky=NSEW)
-        
+        self.frames["MainGame"] = MainGame(self.container, self, selected_palette)
+        self.frames["MainGame"].grid(row=0, column=0, sticky=NSEW)
+        self.show_frame("MainGame")
+
+class MainMenu:
+    '''Set up the GUI'''
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky=NSEW)
+
         for i in range(4):
-            frame.grid_rowconfigure(i, weight=1)
+            self.grid_rowconfigure(i, weight=1)
         for j in range(2):
-            frame.grid_columnconfigure(j, weight=1)            
+            self.grid_columnconfigure(j, weight=1)       
 
-        heading = Label(frame, text="Welcome to the Perfumanator", font="Verdana 16 bold")
+        heading = Label(self, text="Welcome to the Perfumanator", font="Verdana 16 bold")
         heading.grid(row=0, column=0, columnspan=2, sticky=NSEW)
 
-        # photo_label= Label(image="placeholder.png")
-        # photo_label.grid(row=1, column=0, columnspan=2, sticky=NSEW)
-
-        main_menu_text = Label(frame, text="would you like to choose from", font="Verdana 11")
+        main_menu_text = Label(self, text="would you like to choose from", font="Verdana 11")
         main_menu_text.grid(row=2, column=0, columnspan=2, sticky=NSEW, pady=(50, 0))
 
-        free_reign_button = Button(frame, text="Free Reign", bg="lightgreen", font="Verdana 12 bold",command=lambda: self.show_frame("MainGame"))
+        free_reign_button = Button(self, text="Free Reign", bg="lightgreen", font="Verdana 12 bold",command=lambda: self.start_main_game("free_reign"))
         free_reign_button.grid(row=3, column=0, sticky=NSEW)
     
-        preset_palette_button= Button(frame, text="Preset Palette", bg="yellow", font="Verdana 12 bold",command=lambda: self.show_frame("PaletteSelector"))
+        preset_palette_button= Button(self, text="Preset Palette", bg="yellow", font="Verdana 12 bold",command=lambda: self.show_frame("PaletteSelector"))
         preset_palette_button.grid(row=3, column=1, sticky=NSEW)
 
-        return frame
-    
-    def create_palette_selector(self):
-        frame = Frame(self.container)
-        frame.grid(row=0, column=0, sticky=NSEW)
+class PaletteSelector:
+    pass
+    # def create_palette_selector(self):
+    #     frame = Frame(self.container)
+    #     frame.grid(row=0, column=0, sticky=NSEW)
 
-        for i in range(5):
-            frame.grid_rowconfigure(i, weight=1)
-        for j in range(1):
-            frame.grid_columnconfigure(j, weight=1)    
+    #     for i in range(5):
+    #         frame.grid_rowconfigure(i, weight=1)
+    #     for j in range(1):
+    #         frame.grid_columnconfigure(j, weight=1)    
         
-        heading = Label(frame, text="Our Premade Palettes", font="Verdana 16 bold")
-        heading.grid(row=0, column=0, sticky=NSEW)
+    #     heading = Label(frame, text="Our Premade Palettes", font="Verdana 16 bold")
+    #     heading.grid(row=0, column=0, sticky=NSEW)
 
-        summer_palette_button= Button(frame, text="Summer Palette (more fruity scents)", bg="yellow", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
-        summer_palette_button.grid(row=1, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
+    #     summer_palette_button= Button(frame, text="Summer Palette (more fruity scents)", bg="yellow", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
+    #     summer_palette_button.grid(row=1, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
 
-        outdoors_palette_button= Button(frame, text="Outdoors Palette (more woody scents)", bg="forest green", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
-        outdoors_palette_button.grid(row=2, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
+    #     outdoors_palette_button= Button(frame, text="Outdoors Palette (more woody scents)", bg="forest green", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
+    #     outdoors_palette_button.grid(row=2, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
 
-        candy_palette_button= Button(frame, text="Candy Palette (more sweet scents)", bg="hot pink", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
-        candy_palette_button.grid(row=3, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
+    #     candy_palette_button= Button(frame, text="Candy Palette (more sweet scents)", bg="hot pink", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
+    #     candy_palette_button.grid(row=3, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
 
-        zesty_palette_button= Button(frame, text="Zesty Palette (more Citrusy scents)", bg="orange", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
-        zesty_palette_button.grid(row=4, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
+    #     zesty_palette_button= Button(frame, text="Zesty Palette (more Citrusy scents)", bg="orange", font="Verdana 12 bold",command=lambda: ["PLACEHOLDER METHOD(),",self.show_frame("MainGame")])
+    #     zesty_palette_button.grid(row=4, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
 
-        back_button= Button(frame, text="BACK", bg="gray", font="Verdana 12 bold",command=lambda: self.show_frame("MainMenu"))
-        back_button.grid(row=5, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
-        return frame
+    #     back_button= Button(frame, text="BACK", bg="gray", font="Verdana 12 bold",command=lambda: self.show_frame("MainMenu"))
+    #     back_button.grid(row=5, column=0, sticky=NSEW, pady=(10,10), padx=(20,20))
+    #     return frame
 
-    def create_main_game(self):
-        frame = Frame(self.container)
-        frame.grid(row=0, column=0, sticky=NSEW)
-        return frame
-
-
-    def create_checkout(self):
-        frame = Frame(self.container)
-        frame.grid(row=0, column=0, sticky=NSEW)
-        return frame
-    
-    def save_palette_type(self):
-        return 
-
-    
-if __name__ == "__main__":
-    app = PerfumeMaker()
-    app.run()
 
 class MainGame(Frame):
     def __init__(self, root, scent_palette):
@@ -157,7 +150,7 @@ class MainGame(Frame):
             label = Label(selectors_labelframe, text=(f"Scent {i + 1}:"))
             label.grid(row=i * 2, column=0, sticky="W") #x2 because label then combobox on then another label 2 rows down
 
-            combobox = Combobox(selectors_labelframe, textvariable=self.selected_scent_vars[i], values=[""] + scent_palette, state="readonly", width=20)
+            combobox = self.Combobox(selectors_labelframe, textvariable=self.selected_scent_vars[i], values=[""] + scent_palette, state="readonly", width=20)
             combobox.grid(row=i * 2 + 1, column=0, sticky="we", pady=2) #each created combobox is under each label
 
             self.comboboxes.append(combobox) #saves to list
@@ -167,3 +160,10 @@ class MainGame(Frame):
         totals_labelframe.grid(row=1, column=1, sticky="NSEW", padx=10, pady=10)
         totals_labelframe.grid_columnconfigure(0, weight=1)
         totals_labelframe.grid_rowconfigure(0, weight=1)
+
+class Checkout:
+    pass
+
+if __name__ == "__main__":
+    app = FrameManager()
+    app.mainloop()
